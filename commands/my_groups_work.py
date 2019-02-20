f35f7388624da5e9c30081df9498e6559972a406
Nick <nick.young@auckland.ac.nz>
2019-02-21 11:50:37 +1300

    show assigned to, group, requestor

--- commands/my_groups_work.py
+++ commands/my_groups_work.py
@@ -1,16 +1,29 @@
 from pprint import pprint
 from tabulate import tabulate
 
-FIELDS_TO_DISPLAY = ["number", "opened_at", "short_description", "state", "priority", "severity", "category", "sys_created_by"]
+FIELDS_TO_DISPLAY = ["number", "opened_at", "short_description", "state", "priority", "severity", "category", "assigned_to", "assignment_group", "u_requestor"]
 
 def run(BASE_URL, s, args):
-    r = s.get(BASE_URL + "/api/now/v1/table/incident?sysparm_query=assignment_group=javascript:getMyGroups()^active=true^assigned_to=^state!=-5")
+    query = "assignment_group=javascript:getMyGroups()^active=true^assigned_toANYTHING^state!=-5"
+    url = BASE_URL + "/api/now/v1/table/incident"
+    params = {
+        "sysparm_query": query,
+        "sysparm_display_value": "true"
+    }
+    r = s.get(url, params=params)
     r = r.json()
     if 'error' in r:
         print(r['error']['message'])
         return
     results = r['result']
+    #pprint(results)
     filtered_results = []
     for r in results:
-        filtered_results.append([r[key] for key in FIELDS_TO_DISPLAY])
+        filtered_result = []
+        for key in FIELDS_TO_DISPLAY:
+            value = r[key]
+            if type(value) is dict:
+                value = value["display_value"]
+            filtered_result.append(value)
+        filtered_results.append(filtered_result)
     print(tabulate(filtered_results, headers=FIELDS_TO_DISPLAY))
