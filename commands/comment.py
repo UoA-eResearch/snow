6dc1d19e998eee41ffaf89694c80f68de0461e8d
Nick <nick.young@auckland.ac.nz>
2019-02-21 15:44:58 +1300

    ability to post a comment

---
+++ commands/comment.py
@@ -0,0 +1,34 @@
+from pprint import pprint
+
+def run(BASE_URL, s, args):
+    bits = args.split()
+    number = bits[0]
+    args = " ".join(bits[1:])
+    query = "number=" + number
+    url = BASE_URL + "/api/now/table/task"
+    params = {
+        "sysparm_query": query,
+        "sysparm_display_value": "true",
+        "sysparm_fields": "sys_id"
+    }
+    r = s.get(url, params=params)
+    r = r.json()
+    if 'error' in r:
+        print(r["error"]["message"])
+        return
+    if not r['result']:
+        print("Ticket not found")
+        return
+    ticket = r['result'][0]
+    
+    sys_id = ticket["sys_id"]
+
+    data = {
+        "comments": args
+    }
+    url = BASE_URL + "/api/now/table/task/" + sys_id
+    r = s.patch(url, json = data, headers = {"X-no-response-body": "true"})
+    if r.status_code == 204:
+        print("Comment posted successfully")
+    else:
+        print("Unable to post")
